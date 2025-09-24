24c1aa8b7b6c7fb30f81c61d10e77732cf0218bb
Ariav Hayempour <ariavhayempour@gmail.com>
2025-09-24 01:02:44 +0000

    Rolled back to old version

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import { storage } from "./storage";
 
 const app = express();
 app.use(express.json());
@@ -38,13 +37,6 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  // Initialize database with seed data
-  try {
-    await storage.seedData();
-  } catch (error) {
-    console.error("Failed to seed data:", error);
-  }
-  
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
