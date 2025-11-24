103f4fcbbbc2a7f28f1b4bc745123548f33f5dca
Ariav Hayempour <ariavhayempour@gmail.com>
2025-11-23 22:21:20 -0800

    Change screen position when clicking on a truck

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,8 +6,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import MadisonFoodTruckHomePage from "@/pages/home";
 import IndividualFoodTruckDetailPage from "@/pages/truck-detail";
 import PageNotFoundError from "@/pages/not-found";
+import { useEffect } from "react";
 
 function ApplicationRouter() {
+  const [location] = useLocation();
+
+  // Scroll to top whenever the route changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <Switch>
       <Route path="/" component={MadisonFoodTruckHomePage} />
